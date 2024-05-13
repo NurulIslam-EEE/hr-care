8f0691a1c2716b929fb45e9993f33c4eb7862951
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-05-13 16:24:53 +0600

    config file updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCF4Rshjzp4kwImyfDV9m5NC-aaMGzQgCY",
+  authDomain: "hr-care-26c23.firebaseapp.com",
+  projectId: "hr-care-26c23",
+  storageBucket: "hr-care-26c23.appspot.com",
+  messagingSenderId: "614940195580",
+  appId: "1:614940195580:web:57216c38bb69b00e0f7579",
 };
 export default firebaseConfig;
